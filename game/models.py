724e6af28f685a4a0af3546531b510b95301617c
Ismael Guedes <alquemist.dev@gmail.com>
2022-12-05 22:37:37 -0300

    Adicionando modelos básico para o sobrevivente

--- game/models.py
+++ game/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Sobrevivente(models.Model):
+    nome = models.CharField(verbose_name="Nome", max_length=30)
+    idade = models.IntegerField(verbose_name="Idade")
+    sexo_tipos = ('Masculino', 'Feminino')    
+    sexo = models.CharField(verbose_name="Sexo", max_length=10, choices=sexo_tipos)
+    saldo = models.IntegerField(verbose_name="Saldo")
+
+    def __str__(self) -> str:
+        return f"{self.nome} -> {self.saldo} pontos"
+
+class Inventario(models.Model):
+    sobrevivente = models.OneToOneField(Sobrevivente, on_delete=models.CASCADE, verbose_name="Dono")
+
+    def __str__(self) -> str:
+        return f" Inventário({self.id}) do sobrevivente {self.sobrevivente.nome}"
+
+class Recurso(models.Model):
+    descricao = models.CharField(verbose_name="Descrição", max_length=100)
+    quantidade = models.IntegerField(verbose_name="Quantidade")
+    validade = models.DateField(verbose_name="Validade", null=True, blank=True)
+    recurso_tipos = ('Água','Alimento', 'Arma', 'Munição', 'Tecnologia', 'Mapa', 'Item Especial')
+    tipo = models.CharField(verbose_name="Tipo", choices=recurso_tipos)
+    inventario = models.OneToOneField(Inventario, on_delete=models.CASCADE, verbose_name="Inventario")
+
+    def __str__(self) -> str:
+        return f"Item({self.id}) -> {self.descricao}"
