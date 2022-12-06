2dd1609d3252dab5069c94a13f42d435dfbd5d97
Ismael Guedes <alquemist.dev@gmail.com>
2022-12-05 22:57:58 -0300

    Finalizando modelos do sobrevivênte

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from game.models import *
 
 # Register your models here.
+admin.site.register(Sobrevivente)
+admin.site.register(Inventario)
+admin.site.register(Recurso)
--- game/models.py
+++ game/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Sobrevivente(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Usuário")
     nome = models.CharField(verbose_name="Nome", max_length=30)
-    idade = models.IntegerField(verbose_name="Idade")
-    sexo_tipos = ('Masculino', 'Feminino')    
+    datenasc = models.DateField(verbose_name="Data de Nascimento")
+    sexo_tipos = (
+        ('M','Masculino'), 
+        ('F','Feminino')
+    )    
     sexo = models.CharField(verbose_name="Sexo", max_length=10, choices=sexo_tipos)
-    saldo = models.IntegerField(verbose_name="Saldo")
+    saldo = models.IntegerField(verbose_name="Saldo", default=0)
 
     def __str__(self) -> str:
         return f"{self.nome} -> {self.saldo} pontos"
@@ -21,9 +26,17 @@ class Recurso(models.Model):
     descricao = models.CharField(verbose_name="Descrição", max_length=100)
     quantidade = models.IntegerField(verbose_name="Quantidade")
     validade = models.DateField(verbose_name="Validade", null=True, blank=True)
-    recurso_tipos = ('Água','Alimento', 'Arma', 'Munição', 'Tecnologia', 'Mapa', 'Item Especial')
-    tipo = models.CharField(verbose_name="Tipo", choices=recurso_tipos)
-    inventario = models.OneToOneField(Inventario, on_delete=models.CASCADE, verbose_name="Inventario")
+    recurso_tipos = (
+        ('Ar', 'Água'),
+        ('Al', 'Alimento'), 
+        ('Ar', 'Arma'), 
+        ('Mu', 'Munição'), 
+        ('Tc', 'Tecnologia'), 
+        ('Mp', 'Mapa'), 
+        ('Ie', 'Item Especial')
+    )
+    tipo = models.CharField(verbose_name="Tipo", choices=recurso_tipos, max_length=2)
+    inventario = models.ForeignKey(Inventario, on_delete=models.CASCADE, verbose_name="Inventario")
 
     def __str__(self) -> str:
         return f"Item({self.id}) -> {self.descricao}"
