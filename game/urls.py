b3d9fbfa71eb7c7bfd67f0419f3b0a3f72671072
Ismael Guedes <alquemist.dev@gmail.com>
2022-12-10 10:44:09 -0300

    Adicionado método de conexão e desconexão do sobrevivente

--- game/urls.py
+++ game/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from game.views.sobrevivente import *
 
 urlpatterns = [
-    path('listar', listar_sobreviventes, name="listar"),
+    path('conectar', conectar, name="conectar"),
+    path('desconectar', desconectar, name="desconectar"),
+    path('autenticar', autenticar, name="autenticar"),
+    path('listar', listar_sobreviventes, name="inicio"),
 ]
--- game/views/sobrevivente.py
+++ game/views/sobrevivente.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from game.models import Sobrevivente
+
+def conectar(request):
+    return render(request, 'theme/login.html')
+
+def desconectar(request):
+    logout(request)
+    return redirect('inicio')
+
+def autenticar(request):
+    usuario = request.POST['user']
+    senha = request.POST['pass']
+    sobrevivente = authenticate(request, username=usuario, password=senha)
+    if sobrevivente is not None:
+        login(request, sobrevivente)
+        return redirect('inicio')
+    else:
+        return redirect('conectar')
 
 def listar_sobreviventes(request):
     sobreviventes = Sobrevivente.objects.all()
