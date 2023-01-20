c298517186eb4d8850ec188d5c925c384875cc04
Ismael Guedes <alquemist.dev@gmail.com>
2023-01-20 02:37:27 -0300

    Finalizado funcionalidade de mapa

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from game.views.sobrevivente import *
 from game.views.inventario import *
 from game.views.loja import *
+from game.views.mapa import *
 
 urlpatterns = [
     
@@ -20,7 +21,7 @@ urlpatterns = [
     path('inventario/<int:id>/editar', editarRecurso, name="editarRecurso"),
     path('inventario/<int:id>/deletar', removerRecurso, name="removerRecurso"),
 
-    # Loja
+    # Loja:
     path('loja', loja, name="loja"),
     path('ofertar', ofertar, name="ofertar"),
     path('minhas_ofertas', minhasOfertas, name="minhasOfertas"),
@@ -29,8 +30,13 @@ urlpatterns = [
     path('minhas_ofertas/<int:id>/aceitar', aceitarCambio, name="aceitarCambio"),
     path('minhas_ofertas/<int:id>/recusar', recusarCambio, name="recusarCambio"),
 
+    # Cambio:
     path('meus_cambios', meusCambios, name="meusCambios"),
     path('cambio/<int:id>/novo', selecionarOferta, name="cambio"),
     path('cambio/<int:idS>/<int:idP>/confirmar', cambio, name="cambio"),
     path('cambio/<int:id>/deletar', removerCambio, name="removerCambio"),
+
+    # Mapa:
+    path('mapa', mapa, name="mapa"),
+    path('mapa/<str:tipo>/<int:x>/<int:y>/novo', novaRef, name="novaRef"),
 ]
