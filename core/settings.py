6dce3ed228d3cbf3c10fc6395f4374c102587ca8
Ismael Guedes <alquemist.dev@gmail.com>
2023-01-20 23:28:16 -0300

    Feito

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game',
     'rest_framework',
     'django_filters',
-    'game'
 ]
 
 MIDDLEWARE = [
