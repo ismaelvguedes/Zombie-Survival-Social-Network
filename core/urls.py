142d7f1b7e75d3e3493bd8f7447bd267d28d4f50
Ismael Guedes <ismael.basic@gmail.com>
2024-04-10 18:40:19 -0300

    configurando

--- core/urls.py
+++ core/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
 ]
 
 # add at the last
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
