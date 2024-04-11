439cfabd6e7cefb984466878d25afde2708abaf2
Ismael Guedes <ismael.basic@gmail.com>
2024-04-11 00:43:57 -0300

    dominio

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'dephino.com']
 
 
 # Application definition
