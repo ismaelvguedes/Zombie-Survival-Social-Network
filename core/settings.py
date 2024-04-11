226c9bd21c9ccf8993528e95000073e973519999
Ismael Guedes <ismael.basic@gmail.com>
2024-04-10 23:26:50 -0300

    teste novamente

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,6 @@ AUTH_USER_MODEL = 'game.Sobrevivente'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
